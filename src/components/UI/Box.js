92d55172dd685025fa4c849df21272f5599b2f2f
gyojink <ksmug1998@naver.com>
2023-07-06 18:01:20 +0900

    재구축

--- src/components/UI/Box.js
+++
@@ -1,9 +0,0 @@
-import "./Box.css";
-
-const Box = (props) => {
-    const classes = "box " + props.className;
-
-    return <div className={classes}>{props.children}</div>;
-};
-
-export default Box;
