2ed07b60e53866be29f1a74e20a183e98d31cb15
gyojink <ksmug1998@naver.com>
2023-07-06 18:26:51 +0900

    재설계

--- src/App.js
+++ src/App.js
@@ -1,46 +1,11 @@
 import React, { useState } from "react";
 
-import "./App.css";
-import UserInput from "./components/UserForm/UserInput";
-import UserList from "./components/UserList/UserList";
-import Dialog from "./components/Dialog/Dialog";
+import AddUser from "./components/Users/AddUser";
 
 function App() {
-    const [userList, setUserList] = useState([]);
-    const [showAlert, setShowAlert] = useState(false);
-
-    const lastLiftupHandler = (liftedUserData) => {
-        setUserList((prev) => {
-            return [liftedUserData, ...prev];
-        });
-    };
-
-    const showAlertLU = (isVaild) => {
-        if (isVaild.includes(1)) {
-            setShowAlert(true);
-        }
-        console.log("In App.js");
-        console.log(showAlert);
-    };
-
-    let userListContent = null;
-    if (userList.length > 0) {
-        userListContent = <UserList userList={userList} />;
-    }
-
-    let showAlertContent = null;
-    if (showAlert) {
-        showAlertContent = <Dialog showAlert={showAlert} />;
-    }
-
     return (
-        <div className="App">
-            <UserInput
-                onLastLiftup={lastLiftupHandler}
-                onShowAlertLU={showAlertLU}
-            />
-            {userListContent}
-            {showAlertContent}
+        <div>
+            <AddUser />
         </div>
     );
 }
