9554b813d1c7df48373733306dd829d184e39a23
gyojink <ksmug1998@naver.com>
2023-07-08 01:02:58 +0900

    버튼UI 컴포넌트 추가 및 상태 생성, 관리

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../UI/Card";
+import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
 
 const AddUser = () => {
+    const [enteredUsername, setEnteredUsername] = useState("");
+
     const addUserHandler = (event) => {
         event.preventDefault();
+    };
+
+    const usernameChangeHandler = (e) => {
+        setEnteredUsername(e.target.value);
     };
 
     return (
         <Card cssClass={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlForme="username">Username</label>
-                <input id="username" type="text" />
+                <input
+                    id="username"
+                    type="text"
+                    onChange={usernameChangeHandler}
+                />
                 <label htmlForme="age">Age (Years)</label>
                 <input id="age" type="number" />
-                <button type="submit">Add User</button>
+                <Button type="submit">Add User</Button>
             </form>
         </Card>
     );
