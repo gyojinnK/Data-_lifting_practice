2e4557536261d084ae208819add634bf165399e8
gyojink <ksmug1998@naver.com>
2023-07-08 01:01:56 +0900

    버튼 태그 및 속성 추가

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -2,8 +2,16 @@ import React from "react";
 
 import classes from "./Button.module.css";
 
-const Button = () => {
-    return <button></button>;
+const Button = (props) => {
+    return (
+        <button
+            className={classes.button}
+            type={props.type || "button"}
+            onClick={props.onClick}
+        >
+            {props.children}
+        </button>
+    );
 };
 
 export default Button;
