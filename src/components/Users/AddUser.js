8b3424025f69d79f03d5757de54c48260c1bb48a
gyojink <ksmug1998@naver.com>
2023-07-09 01:25:27 +0900

    유저 정보(객체) Lifting 및 에러 관리

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,32 +3,80 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
+import ErrorModal from "../UI/ErrorModal";
 
-const AddUser = () => {
+const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState("");
+    const [enteredUserage, setEnteredUserage] = useState("");
+    const [error, setError] = useState();
 
     const addUserHandler = (event) => {
         event.preventDefault();
+        if (
+            enteredUsername.trim().length === 0 ||
+            enteredUserage.trim().length === 0
+        ) {
+            setError({
+                title: "Invalid input",
+                message:
+                    "Please enter a valid name and age (non-empty values).",
+            });
+            return;
+        }
+        if (+enteredUserage < 1) {
+            // +기호를 사용하여 문자열을 숫자형으로 변경
+            setError({
+                title: "Invalid age",
+                message: "Please enter a valid age (> 0).",
+            });
+            return;
+        }
+        props.onAddUser(enteredUsername, enteredUserage);
+        setEnteredUsername("");
+        setEnteredUserage("");
     };
 
     const usernameChangeHandler = (e) => {
         setEnteredUsername(e.target.value);
     };
 
+    const userageChangeHandler = (e) => {
+        setEnteredUserage(e.target.value);
+    };
+
+    const errorHandler = () => {
+        setError(null);
+    };
+
     return (
-        <Card cssClass={classes.input}>
-            <form onSubmit={addUserHandler}>
-                <label htmlForme="username">Username</label>
-                <input
-                    id="username"
-                    type="text"
-                    onChange={usernameChangeHandler}
+        <div>
+            {error && (
+                <ErrorModal
+                    title={error.title}
+                    message={error.message}
+                    onErrorHandler={errorHandler}
                 />
-                <label htmlForme="age">Age (Years)</label>
-                <input id="age" type="number" />
-                <Button type="submit">Add User</Button>
-            </form>
-        </Card>
+            )}
+            <Card cssClass={classes.input}>
+                <form onSubmit={addUserHandler}>
+                    <label htmlFor="username">Username</label>
+                    <input
+                        id="username"
+                        type="text"
+                        value={enteredUsername}
+                        onChange={usernameChangeHandler}
+                    />
+                    <label htmlFor="age">Age (Years)</label>
+                    <input
+                        id="age"
+                        type="number"
+                        value={enteredUserage}
+                        onChange={userageChangeHandler}
+                    />
+                    <Button type="submit">Add User</Button>
+                </form>
+            </Card>
+        </div>
     );
 };
 
